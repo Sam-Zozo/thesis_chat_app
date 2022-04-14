9c51c678ff60a1aa1824d73b085ae34391296f5c
Sam Orozco <orozcosam2000@gmail.com>
2022-04-15 03:36:11 +0800

    add cors

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret#'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/')
 def index():
